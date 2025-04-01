688546444054d2af24e1954d4bcee9ad65258ca1
kasperwikman <kasperwikmanhki@gmail.com>
2025-04-01 08:03:44 +0000

    import psycopg and dotenv

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import uvicorn
+import psycopg
+from psycopg.rows import dict_row
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
